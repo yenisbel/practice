4908e5faabfa38c6da52986d8906de4d4c31e084
Yenisbel Valle <yenisbelv@gmail.com>
2020-06-08 22:19:43 -0700

    Add

--- leetcode/strings.js
+++ leetcode/strings.js
@@ -29,3 +29,43 @@ if(carry !== 0) {
 return result;
   
 };
+
+// License Key Formatting
+
+var licenseKeyFormatting = function(S, K) {
+  let expression = RegExp('/-/g');
+  let arrClean = S.toUpperCase().replace(/-/g, '').split('');
+  for(let i = arrClean.length - 1 - K; i >= 0; i-=K ){
+      arrClean[i] = `${arrClean[i]}-`
+  }
+
+  return arrClean.join('')
+  
+};
+
+//You are given a string that represents time in the format hh:mm. Some of the digits are blank (represented by ?). Fill in ? such that the time represented by this string is the maximum possible. 
+//Maximum time: 23:59, minimum time: 00:00. You can assume that input string is always valid.
+
+function maxTime(str){
+  let[hours, minutes] = str.split(':');
+  if(hours[0] === '?'){
+    if (hours[1] <= '3' || hours[1] === '?'){
+      hours[0] = '2'
+    }else{
+      hours[0] ='1'
+    }
+  };
+  if(hours[1] === '?'){
+    if (hours[0] == '2'){
+      result[1] = '3'
+    }else{
+      '9'
+    }
+  };
+
+  result[2] = (minutes[0] === '?')? '9': minutes[0];
+  result[3] = (minutes[1] === '?')? '5': minutes[1];
+  // console.log(`${hours}:${minutes}`)
+  // return `${hours}:${minutes}`
+  return `${hours}:${minutes}`
+}
